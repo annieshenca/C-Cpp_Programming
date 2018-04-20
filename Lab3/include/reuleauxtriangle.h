736c04dc01a6821b70bb7650fb7f27cf75a14e8e
Annie Shen <annieshenca@gmail.com>
2018-04-19 21:51:20 -0700

    added circle in circle tests x4

---
+++ Lab3/include/reuleauxtriangle.h
@@ -0,0 +1,32 @@
+/*
+ * Annie Shen (ashen7)
+ * CMPS 109 - Spring 2018
+ * Lab 3 - Due Apr 22, 2018
+ */
+
+#ifndef _REULEAUXTRIANGLE_H_
+#define _REULEAUXTRIANGLE_H_
+
+#include <vector>
+#include "containable.h"
+#include "point.h"
+#include "line.h"
+#include "circle.h"
+#include "polygon.h"
+
+class ReuleauxTriangle : public Containable2D {
+    private:
+        std::vector<Point2D> vertices_;
+
+    public:
+        ReuleauxTriangle(std::vector<Point2D> vertices);
+
+        std::vector<Point2D> vertices();
+        // std::vector<Line>;
+
+        bool containedWithIn(Circle &circle);
+        bool containedWithIn(RegularConvexPolygon &polygon);
+        bool containedWithIn(ReuleauxTriangle &rt);
+};
+
+#endif
--- Lab3/src/main.cc
+++ Lab3/src/main.cc
@@ -22,22 +22,39 @@ static void results (const char *msg, bool expected, bool got)
         std::cout << "FAIL\n";
 }
 
-/*
- *
+/**************************************************************************
+ * Test cases for CIRCLE OUTER.
  */
-static void testCirclesInCircle()
-{
-    Circle inner = Circle(Point2D(0.0,0.0), 4.0);
+static void testCirclesInCircle1() {
     Circle outer = Circle(Point2D(0.0,0.0), 8.0);
+    Circle inner = Circle(Point2D(0.0,0.0), 4.0);
 
     results("Circle-in-Circle: ", true, inner.containedWithin(outer));
 }
 
-/*
- *
- */
-static void testTrianglesInCircle()
-{
+static void testCirclesInCircle2() {
+    Circle outer = Circle(Point2D(0.0,-1.0), 8.0);
+    Circle inner = Circle(Point2D(2.0,2.0), 4.0);
+
+    results("Circle-in-Circle: ", true, inner.containedWithin(outer));
+}
+
+static void testCirclesInCircle3() {
+    Circle outer = Circle(Point2D(1.0,-3.33), 8.0);
+    Circle inner = Circle(Point2D(6.0,2.021), 4.0);
+
+    results("Circle-in-Circle: ", false, inner.containedWithin(outer));
+}
+
+static void testCirclesInCircle4() {
+    Circle outer = Circle(Point2D(0.0,0.0), 8.0);
+    Circle inner = Circle(Point2D(12.0,0.0), 5.0); // Touch the side outside
+
+    results("Circle-in-Circle: ", false, inner.containedWithin(outer));
+}
+//==========================================//
+
+static void testTrianglesInCircle() {
     Circle outer = Circle(Point2D(0.0,0.0), 8.0);
     RegularConvexPolygon inner =
         RegularConvexPolygon({ Point2D(-1,-1), Point2D(0,2), Point2D(1,-1)});
@@ -45,11 +62,7 @@ static void testTrianglesInCircle()
     results("Triangle-in-Circle: ", true, inner.containedWithin(outer));
 }
 
-/*
- *
- */
-static void testTrianglesInCircle()
-{
+static void testTrianglesInCircle() {
     Circle outer = Circle(Point2D(0.0,0.0), 8.0);
     RegularConvexPolygon inner =
         RegularConvexPolygon({ Point2D(-1,-1), Point2D(0,2), Point2D(1,-1)});
@@ -57,29 +70,52 @@ static void testTrianglesInCircle()
     results("Triangle-in-Circle: ", true, inner.containedWithin(outer));
 }
 
+static void testTrianglesInCircle() {
+    Circle outer = Circle(Point2D(0.0,0.0), 8.0);
+    RegularConvexPolygon inner =
+        RegularConvexPolygon({ Point2D(-1,-1), Point2D(0,2), Point2D(1,-1)});
+
+    results("Triangle-in-Circle: ", true, inner.containedWithin(outer));
+}
+
+static void testTrianglesInCircle() {
+    Circle outer = Circle(Point2D(0.0,0.0), 8.0);
+    RegularConvexPolygon inner =
+        RegularConvexPolygon({ Point2D(-1,-1), Point2D(0,2), Point2D(1,-1)});
+
+    results("Triangle-in-Circle: ", true, inner.containedWithin(outer));
+}
+
+/**************************************************************************
+ * Test cases for POLYGON OUTER.
+ */
 
 
 
 
+/**************************************************************************
+ * Test cases for RTRIANGLE OUTER.
+ */
+// static void testTrianglesInRTriangle()
+// {
+//
+//     RegularConvexPolygon inner =
+//         RegularConvexPolygon({ Point2D(-1,-1), Point2D(0,2), Point2D(1,-1)});
+//
+//     results("Triangle-in-Circle: ", true, inner.containedWithin(outer));
+// }
 
 
 
-
-
-
-
-
-
-
-
-
-
-/*
- *
+/**************************************************************************
+ * Main function. Contains all tests for shape functions.
  */
 int main(int argc, char *argv[])
 {
-    testCirclesInCircle();
+    testCirclesInCircle1();
+    testCirclesInCircle2();
+    testCirclesInCircle3();
+    testCirclesInCircle4();
     testTrianglesInCircle();
 }
 

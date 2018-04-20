4ee116316b13a0750d2fb048f7f6b8d5d45094f8
Annie Shen <annieshenca@gmail.com>
2018-04-19 19:42:43 -0700

    wip main.cc

--- Lab3/src/main.cc
+++ Lab3/src/main.cc
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2018 David C. Harrison. All right reserved.
  *
- * You may not use, distribute, publish, or modify this code without 
+ * You may not use, distribute, publish, or modify this code without
  * the express written permission of the copyright holder.
  */
 
@@ -10,6 +10,9 @@
 #include "circle.h"
 #include "polygon.h"
 
+/* Pass in msg, expected result, and what the function actually return.
+ * Returns PASS or FAIL for each test.
+ */
 static void results (const char *msg, bool expected, bool got)
 {
     std::cout << msg;
@@ -19,25 +22,78 @@ static void results (const char *msg, bool expected, bool got)
         std::cout << "FAIL\n";
 }
 
+/*
+ *
+ */
 static void testCirclesInCircle()
 {
     Circle inner = Circle(Point2D(0.0,0.0), 4.0);
     Circle outer = Circle(Point2D(0.0,0.0), 8.0);
-    
+
     results("Circle-in-Circle: ", true, inner.containedWithin(outer));
 }
 
+/*
+ *
+ */
 static void testTrianglesInCircle()
 {
     Circle outer = Circle(Point2D(0.0,0.0), 8.0);
     RegularConvexPolygon inner =
         RegularConvexPolygon({ Point2D(-1,-1), Point2D(0,2), Point2D(1,-1)});
-    
+
     results("Triangle-in-Circle: ", true, inner.containedWithin(outer));
 }
 
-int main(int argc, char *argv[]) 
+/*
+ *
+ */
+static void testTrianglesInCircle()
+{
+    Circle outer = Circle(Point2D(0.0,0.0), 8.0);
+    RegularConvexPolygon inner =
+        RegularConvexPolygon({ Point2D(-1,-1), Point2D(0,2), Point2D(1,-1)});
+
+    results("Triangle-in-Circle: ", true, inner.containedWithin(outer));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ *
+ */
+int main(int argc, char *argv[])
 {
     testCirclesInCircle();
     testTrianglesInCircle();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//

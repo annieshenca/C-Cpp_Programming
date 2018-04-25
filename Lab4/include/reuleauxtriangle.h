b08b8526f85a5f32202979f0d34bd41f2e79a63c
Administrator <ashen7@ucsc.edu>
2018-04-25 12:46:49 -0700

    adding Lab4 base files

---
+++ Lab4/include/reuleauxtriangle.h
@@ -0,0 +1,34 @@
+/*
+ * Annie Shen (ashen7)
+ * CMPS 109 - Spring 2018
+ * Lab 3 - Due Apr 22, 2018
+ */
+
+#ifndef _REULEAUXTRIANGLE_H_
+#define _REULEAUXTRIANGLE_H_
+
+#include <vector>
+#include "containable.h"
+#include "point.h"
+#include "line.h"
+#include "circle.h"
+#include "polygon.h"
+
+class ReuleauxTriangle : public Containable2D {
+    private:
+        std::vector<Point2D> ver_;
+
+    public:
+        //ReuleauxTriangle(std::vector<Point2D> vertices);
+        ReuleauxTriangle(Point2D vertices[3]);
+
+        // std::vector<Point2D> vertices();
+        // std::vector<Line> edges();
+        //std::vector<Circle>???;
+
+        bool containedWithin(Circle &circle);
+        bool containedWithin(RegularConvexPolygon &polygon);
+        bool containedWithin(ReuleauxTriangle &rt);
+};
+
+#endif

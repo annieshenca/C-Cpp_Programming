3238e6affc833b3a44cc1d66a351db3e2d88ec74
Administrator <ashen7@ucsc.edu>
2018-05-12 23:40:22 -0700

    paste in Lab5 code

--- Lab6/src/radix.cc
+++ Lab6/src/radix.cc
@@ -1,13 +1,36 @@
 /*
  * Copyright (C) 2018 David C. Harrison. All right reserved.
  *
- * You may not use, distribute, publish, or modify this code without 
+ * You may not use, distribute, publish, or modify this code without
  * the express written permission of the copyright holder.
  */
 
 #include "radix.h"
+#include <iostream>
+#include <string>
+#include <thread>
+#include <algorithm>
+#include <vector>
 
-void ParallelRadixSort::msd(std::vector<std::reference_wrapper<std::vector<unsigned int>>> &lists, unsigned int cores) { 
-    int ununsed; // to fail compilke warning test, remove
-    // your implementation goes here :)
+void ParallelRadixSort::msd(std::vector<std::reference_wrapper<std::vector<unsigned int>>> &lists, unsigned int cores) {
+    std::vector<std::thread*> threads;
+    unsigned int num_threads = 0;
+
+    // Iterating through the vectors in the vector
+    for(std::vector<unsigned int> &list : lists) {
+        // sort( vec.data(), vec.size() );
+        threads.push_back(new std::thread{[&list] {
+            std::sort(list.begin(), list.end(), [](const unsigned int &a, const unsigned int &b) {
+                return std::to_string(a).compare(std::to_string(b)) < 0;
+            });
+        }});
+        num_threads++;
+
+        if (threads.size() == cores || num_threads == lists.size()) {
+            for (std::thread *thread : threads) {
+                thread->join();
+            }
+            threads.clear();
+        }
+    }
 }
